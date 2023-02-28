e93b3077866f3dc96184c2dbd20a17e9ea5c8ef8
Clayton Engle <123935116+Clayton-Engle-2@users.noreply.github.com>
2023-02-28 09:27:50 -0500

    Update CalculatorGUI.java

--- src/gui/CalculatorGUI.java
+++ src/gui/CalculatorGUI.java
@@ -1,73 +1,124 @@
-public class CalculatorGUI extends JFrame {
-    private JLabel resultLabel;
+package gui;
 
-    public CalculatorGUI() {
-        super("Calculator");
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-        // Set the size of the window to 960 x 1080
-        setSize(new Dimension(1920, 1060));
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-        // Create the result label
-        resultLabel = new JLabel("0", SwingConstants.RIGHT);
-        resultLabel.setPreferredSize(new Dimension(1000, 100));
-        resultLabel.setFont(new Font("Arial", Font.BOLD, 36));
-        resultLabel.setBackground(Color.WHITE);
-        resultLabel.setOpaque(true);
-     // Create a label to hold the image
-        JLabel label = new JLabel();
+import control.Pipe;
+import main.data.types.TripleString;
 
-        // Load the image from a file
-        BufferedImage image = loadImageFromFile("example.jpg");
+public class CalculatorGUI implements ActionListener {
+	private JFrame frame;
+    private JLabel label;
+    private JTextField textFieldA, textFieldB;
+    private Pipe<String[]> controlIn; 
+	private Pipe<TripleString> controlOut;
+    private String LastButton;
 
-        // Set the image on the label
-        label.setIcon(new ImageIcon(image));
+    public CalculatorGUI( JFrame frame,Pipe<String[]> cIn, Pipe<TripleString> cOut) {
+    	 this.frame = frame;
+    	this.controlIn = cIn;
+		this.controlOut = cOut;
+		
+    }
+     
+    public void start() {
+        frame.setSize(1920, 1080);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Add the label to the frame
-        resultLabel.add(label);
-       
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
 
-        // Create the button panel
+        label = new JLabel("Welcome to My GUI");
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setFont(new Font("Arial", Font.BOLD, 30));
+        panel.add(label, BorderLayout.NORTH);
+        
+        JPanel answer = new JPanel();
+        answer.setLayout(new BorderLayout());
+
+        JLabel solution = new JLabel("Solution: ");
+        solution.setHorizontalAlignment(JLabel.CENTER);
+        solution.setFont(new Font("Arial", Font.BOLD, 30));
+        answer.add(solution, BorderLayout.NORTH);
+        panel.add(answer);
+        
+
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(5, 8, 5, 5));
-        buttonPanel.setPreferredSize(new Dimension(1910, 410));
-        buttonPanel.setBackground(Color.LIGHT_GRAY);
-
-        // Create the buttons
-        JButton[] buttons = new JButton[40];
-        String[] buttonLabels = { "freq", "insct", "avg", "!", "C", "CE", "<-", "/", 
-        						  "P(A|B)", "union", "med", "sqrt", "7", "8", "9", "*", 
-        						  "bayes", "cplmt", "mode", "expo", "4", "5", "6", "-", 
-        						  ")", "nPr", "var", "absV", "1", "2", "3", "+", 
-        						  "]", "nCr", "sdev", "mod", "+/-", "0", ".", "=" };
-        for (int i = 0; i < buttons.length; i++) {
-            buttons[i] = new JButton(buttonLabels[i]);
-            buttons[i].setFont(new Font("Arial", Font.PLAIN, 24));
-            buttons[i].setBackground(Color.BLACK);
-            buttons[i].setForeground(Color.WHITE);
-            buttonPanel.add(buttons[i]);
+        buttonPanel.setLayout(new GridLayout(4, 5));
+        String[] buttonNames = {"1", "intersect", "3", "+", "A", "4", "5", "6", "-", "B", "7", "8", "9", "*", "C", ".", "0", "=", "/", "D"};
+        String[] buttonDescs = {"Button 1", "Button 2", "Button 3", "Add", "Set A", "Button 4", "Button 5", "Button 6", "Subtract", "Set B", "Button 7", "Button 8", "Button 9", "Multiply", "Clear", "Decimal", "Button 0", "Equal", "Divide", "Delete"};
+        for (int i = 0; i < buttonNames.length; i++) {
+            JButton button = new JButton(buttonNames[i]);
+            button.setToolTipText(buttonDescs[i]);
+            button.addActionListener(this);
+            buttonPanel.add(button);
         }
+        panel.add(buttonPanel, BorderLayout.SOUTH);
 
-        // Add the components to the window
-        JPanel contentPane = new JPanel();
-        contentPane.setLayout(new BorderLayout(5, 5));
-        contentPane.setBackground(Color.GRAY);
-        contentPane.add(resultLabel, BorderLayout.NORTH);
-        contentPane.add(buttonPanel, BorderLayout.SOUTH);
-        setContentPane(contentPane);
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.WEST;
+        c.insets = new Insets(10, 10, 10, 10);
+        c.gridx = 0;
+        c.gridy = 0;
+        JLabel labelA = new JLabel("Set A:");
+        labelA.setFont(new Font("Arial", Font.PLAIN, 20));
+        inputPanel.add(labelA, c);
+        c.gridx = 1;
+        textFieldA = new JTextField(20);
+        textFieldA.setFont(new Font("Arial", Font.PLAIN, 20));
+        inputPanel.add(textFieldA, c);
+        c.gridx = 0;
+        c.gridy = 1;
+        JLabel labelB = new JLabel("Set B:");
+        labelB.setFont(new Font("Arial", Font.PLAIN, 20));
+        inputPanel.add(labelB, c);
+        c.gridx = 1;
+        textFieldB = new JTextField(20);
+        textFieldB.setFont(new Font("Arial", Font.PLAIN, 20));
+        inputPanel.add(textFieldB, c);
+        panel.add(inputPanel, BorderLayout.WEST);
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setResizable(false);
-        setVisible(true);
-    }
-    
-    private static BufferedImage loadImageFromFile(String filename) {
-        BufferedImage image = null;
-        try {
-            image = ImageIO.read(new File(filename));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return image;
+        frame.setContentPane(panel);
+        frame.setVisible(true);
     }
 
+    public void actionPerformed(ActionEvent e) {
+    	
+        String buttonName = ((JButton) e.getSource()).getText();
+        String buttonDesc = ((JButton) e.getSource()).getToolTipText();
+        if (buttonName.equals("=")) {
+        	System.out.println(" flag");
+            String setA = textFieldA.getText();
+            String setB = textFieldB.getText();
+            String opperation = LastButton;
+            TripleString info = new TripleString(setA, setB, opperation);
+            controlOut.put(info);
+            String answer = null;
+            while(answer == null) {
+            	if(controlIn.hasInput())
+            		answer = controlIn.take()[0];
+            }
+            System.out.println(answer);
+            label.setText(answer);
+            
+            
+        }
+        LastButton = buttonName;
+        label.setText(buttonName + " - " + buttonDesc);
+    }
+}
