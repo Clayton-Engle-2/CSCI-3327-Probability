6e0dc1dcf14507f5617be5699621157ca3c674c4
Clayton Engle <123935116+Clayton-Engle-2@users.noreply.github.com>
2023-03-14 10:47:12 -0400

    Add files via upload

---
+++ src/control/tasks/runnable/binomial/BiDistroAtLeast.java
@@ -0,0 +1,69 @@
+package control.tasks.runnable.binomial;
+
+import control.io.Pipe;
+import control.tasks.tasksuper.Task;
+import main.data.types.numbers.DoubleAndString;
+import main.data.types.numbers.ShareData;
+import model.statslibrary.StatLibrary;
+
+public class BiDistroAtLeast extends Task implements Runnable{
+	
+	private boolean isReady;
+	private long memoryNeeded;
+	private long actions;;
+	
+	private double[] data;
+	private Pipe<ShareData> toControl;
+	private StatLibrary stat;
+	
+	
+	public BiDistroAtLeast(double[] number, Pipe<ShareData> pipe) {
+		super(number, pipe);
+		data = number;
+		toControl = pipe;
+		stat= new StatLibrary();
+	}
+
+	@Override
+	public void run() {
+		
+		double probability = -1;
+			
+		try {
+			probability = stat.binomialDistributionAtLeast(data[0], data[1], data[2]);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+			
+		ShareData ret = new DoubleAndString(probability, "biDistroAtLeast");
+		toControl.put(ret);
+	}
+
+	@Override
+	public void isReady(boolean canRun) {
+		isReady = canRun;	
+	}
+
+	@Override
+	public boolean isReady() {
+		return isReady;
+	}
+	@Override
+	public long getMemoryNeeded() {
+		return memoryNeeded;
+	}
+	@Override
+	public void setMemoryNeeded(long memoryNeeded) {
+		this.memoryNeeded = memoryNeeded;
+	}
+	@Override
+	public long getActions() {
+		return actions;
+	}
+	@Override
+	public void setActions(long actions) {
+		this.actions = actions;
+	}
+
+}
+
