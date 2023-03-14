94bd2a29d20ea8426b3755b2ecabdcd66ba2fa41
Clayton Engle <123935116+Clayton-Engle-2@users.noreply.github.com>
2023-03-14 12:35:07 -0400

    Add files via upload

---
+++ src/control/io/Pipe.java
@@ -0,0 +1,37 @@
+package control.io;
+
+public class Pipe<T> {
+
+	private ThreadQueue<T> queue;
+
+	public Pipe() {
+		this.queue = new ThreadQueue<T>(4);
+	}
+	
+	public boolean hasInput() {
+		if(!queue.isEmpty())
+			return true;
+		else
+			return false;
+	}
+
+	public void put(T item) {
+		try {
+			this.queue.put(item);
+		} catch (InterruptedException e) {			
+			e.printStackTrace();
+		}
+	}
+
+	public T take() {
+		try {
+			return this.queue.take();
+		} catch (InterruptedException e) {			
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+
+
+}
