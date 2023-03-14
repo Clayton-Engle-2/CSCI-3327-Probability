763f0dadb2cc1f079b867ecb0a506d4ff2ead0e6
Clayton Engle <123935116+Clayton-Engle-2@users.noreply.github.com>
2023-03-14 10:48:48 -0400

    Add files via upload

---
+++ src/control/tasks/runnable/conditional/Combination.java
@@ -0,0 +1,90 @@
+package control.tasks.runnable.conditional;
+
+import java.math.BigInteger;
+
+import control.io.Pipe;
+import control.tasks.tasksuper.Task;
+import main.data.types.numbers.BigString;
+import main.data.types.numbers.ShareData;
+import model.statslibrary.StatLibrary;
+
+public class Combination extends Task implements Runnable{
+	
+	private boolean isReady;
+	private long memoryNeeded;
+	private long actions;;
+	
+	private double data1;
+	private double data2;
+	private Pipe<ShareData> toControl;
+	private StatLibrary stat;
+	
+	
+	public Combination(double number1, double number2, Pipe<ShareData> pipe) {
+		super(number1, number2,  pipe);
+		data1 = number1;
+		data2 = number2;
+		toControl = pipe;
+		stat= new StatLibrary();
+	}
+
+	@Override
+	public void run() {
+		
+		BigInteger combo = null;
+			
+		try {
+			combo = stat.combination(data1, data2);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+			
+		ShareData ret = new BigString(combo, "Combination");
+		toControl.put(ret);
+	}
+
+	@Override
+	public void isReady(boolean canRun) {
+		isReady = canRun;	
+	}
+
+	@Override
+	public boolean isReady() {
+		return isReady;
+	}
+	@Override
+	public long getMemoryNeeded() {
+		return memoryNeeded;
+	}
+	@Override
+	public void setMemoryNeeded(long memoryNeeded) {
+		this.memoryNeeded = memoryNeeded;
+	}
+	@Override
+	public long getActions() {
+		return actions;
+	}
+	@Override
+	public void setActions(long actions) {
+		this.actions = actions;
+	}
+
+	public double getData1() {
+		return data1;
+	}
+
+	public void setData1(double data1) {
+		this.data1 = data1;
+	}
+
+	public double getData2() {
+		return data2;
+	}
+
+	public void setData2(double data2) {
+		this.data2 = data2;
+	}
+
+}
+
+
---
+++ src/control/tasks/runnable/conditional/Factorial.java
@@ -0,0 +1,71 @@
+package control.tasks.runnable.conditional;
+
+import java.math.BigInteger;
+
+import control.io.Pipe;
+import control.tasks.tasksuper.Task;
+import main.data.types.numbers.BigString;
+import main.data.types.numbers.ShareData;
+import model.statslibrary.StatLibrary;
+
+public class Factorial extends Task implements Runnable{
+	
+	private boolean isReady;
+	private long memoryNeeded;
+	private long actions;;
+	
+	private double data;
+	private Pipe<ShareData> toControl;
+	private StatLibrary stat;
+	
+	
+	public Factorial(double number, Pipe<ShareData> pipe) {
+		super(number, pipe);
+		data = number;
+		toControl = pipe;
+		stat= new StatLibrary();
+	}
+
+	@Override
+	public void run() {
+		
+		BigInteger facto = null;
+			
+		try {
+			facto = stat.factorial(data);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+			
+		ShareData ret = new BigString(facto, "factorial");
+		toControl.put(ret);
+	}
+
+	@Override
+	public void isReady(boolean canRun) {
+		isReady = canRun;	
+	}
+
+	@Override
+	public boolean isReady() {
+		return isReady;
+	}
+	@Override
+	public long getMemoryNeeded() {
+		return memoryNeeded;
+	}
+	@Override
+	public void setMemoryNeeded(long memoryNeeded) {
+		this.memoryNeeded = memoryNeeded;
+	}
+	@Override
+	public long getActions() {
+		return actions;
+	}
+	@Override
+	public void setActions(long actions) {
+		this.actions = actions;
+	}
+
+}
+
